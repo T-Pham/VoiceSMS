97e4a14b2921b6d110274f71f7d0eaaf15c84994
Thanh Pham <thanh.pham@eastagile.com>
2013-08-01 11:49:26 +0700

    Initial Commit

---
+++ VoiceSMS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VoiceSMS
+//
+//  Created by eastagile 87:21 on 8/1/13.
+//  Copyright (c) 2013 eastagile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VoiceSMS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VoiceSMS
+//
+//  Created by eastagile 87:21 on 8/1/13.
+//  Copyright (c) 2013 eastagile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
